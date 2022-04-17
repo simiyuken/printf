507c11db803d8f8ac2e3f96d2bb479ab5f493212
Kennedy Simiyu <mukhebisimiyu@gmail.com>
2022-04-17 15:32:02 -0700

    upated main.h

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
-#ifndef HEADER_MAIN
-#define HEADER_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
+
+int _write(char c);
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
@@ -19,4 +21,5 @@ int print_base16_upper_lower(va_list arg, char *representation);
 int loop_format(va_list arg, const char *string);
 int call_function_manager(char aux, va_list arg);
 int check_percent(int *flag, char aux);
+
 #endif
